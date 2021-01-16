feabee89159cffa7a88714b16de69c22283992bf
日高隼人 <hidakahayato@hidakahayatonoMacBook-Pro.local>
2021-01-17 01:41:08 +0900

    first commit

---
+++ todo3/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  todo3
+//
+//  Created by 日高隼人 on 2021/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    var todoList = [String]()
+
+    let userDefaults = UserDefaults.standard
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 追記：データ読み込み
+        if let storedTodoList = userDefaults.array(forKey: "todoList") as? [String] {
+            todoList.append(contentsOf: storedTodoList)
+        }
+    }
+
+
+    @IBAction func addBtnAction(_ sender: Any) {
+        let alertController = UIAlertController(title: "ToDo追加", message: "ToDoを入力してください。", preferredStyle: UIAlertController.Style.alert)
+        alertController.addTextField(configurationHandler: nil)
+
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (acrion: UIAlertAction) in
+            //OKをタップした時の処理
+            if let textField = alertController.textFields?.first {
+                self.todoList.insert(textField.text!, at: 0)
+                self.tableView.insertRows(at: [IndexPath(row :0, section: 0)], with: UITableView.RowAnimation.right)
+
+                self.userDefaults.set(self.todoList,forKey: "todoList")
+            }
+
+        }
+
+        alertController.addAction(okAction)
+
+        let cancelButton = UIAlertAction(title: "CANCEL", style: UIAlertAction.Style.cancel, handler: nil)
+        alertController.addAction(cancelButton)
+        present(alertController, animated: true, completion: nil)
+    }
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+
+    // セルの中身を設定
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        let todoTitle = todoList[indexPath.row]
+        cell.textLabel?.text = todoTitle
+        return cell
+    }
+
+    // セルの削除機能
+
+    // 追加：セルの削除機能
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            print(todoList)
+            todoList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
+
+            userDefaults.set(todoList, forKey:"todoList")
+        }
+    }
+}
+
+
+
